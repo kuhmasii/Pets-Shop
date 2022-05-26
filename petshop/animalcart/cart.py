fb833be25db9c07c4eea3c96e8825196639e0b58
kuhmasii <olaisaiah54@gmail.com>
2022-05-26 12:55:12 +0100

    refactor: uncomment methods in cart file

--- petshop/animalcart/cart.py
+++ petshop/animalcart/cart.py
@@ -63,9 +63,9 @@ class Cart(object):
         self.session.modified = True
 
     def remove(self, product):
-        '''
+        """
         Remove a product from the cart.
-        '''
+        """
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
