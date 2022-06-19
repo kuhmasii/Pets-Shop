9211c974efbbf720def068fdd4a4f4dbde0d1550
Eyitayo Olonilua <oloniluaeyitayo@gmail.com>
2022-06-19 14:16:40 -0700

    added cart and Product Page

---
+++ frontend/product_page/product_page.js
@@ -0,0 +1,28 @@
+let dropDown = document.querySelector('.drop_down');
+let shopLink = document.querySelector('.shop_link');
+let homeLink = document.querySelector('.home_link');
+let featuredLink = document.querySelector('.featured_link');
+let landingHero = document.querySelector('.product_page_landing_hero');
+
+
+shopLink.onmouseover = () => {
+    dropDown.classList.add('active');
+}
+
+dropDown.onmouseover = () => {
+    dropDown.classList.add('active');
+}
+
+homeLink.onmouseover = () => {
+    dropDown.classList.remove('active');
+}
+
+
+featuredLink.onmouseover = () => {
+    dropDown.classList.remove('active');
+}
+
+
+landingHero.onmouseover = () => {
+    dropDown.classList.remove('active');
+}
