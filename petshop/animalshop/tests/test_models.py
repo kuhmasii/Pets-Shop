fe1d9eff213dacb68d4c0824aacf5a48c0a166f2
kuhmasii <olaisaiah54@gmail.com>
2022-07-04 16:04:53 +0100

    feat: wrote test for models file

--- petshop/animalshop/tests/test_models.py
+++ petshop/animalshop/tests/test_models.py
@@ -1,5 +1,3 @@
-from sre_constants import CATEGORY_LOC_NOT_WORD
-from unicodedata import category
 from django.test import TestCase
 from animalshop.models import Product, Category
 
@@ -73,3 +71,81 @@ class CategoryTestCase(TestCase):
         self.assertTrue(obj.get_products())
         self.assertQuerysetEqual(
             obj.get_products(), Product.objects.filter(category=obj))
+
+
+class ProductTestcase(TestCase):
+    def setUp(self) -> None:
+        cat = Category.objects.create(name='Testing Category')
+        Product.objects.create(
+            category=cat, name='Testing Product',
+            slug='testing-product',
+            price=100.00,
+            _discount=50.00)
+
+    def test_product_object_created(self):
+        """
+        Test checks an object of Product was 
+        sucessfully created.
+        """
+        obj = Product.objects.get(pk=1)
+        check = 'Testing Product'
+
+        self.assertIsInstance(obj, Product)
+        self.assertEqual(obj.__str__(), check)
+
+    def test_product_object_not_created(self):
+        """
+        Test checks if an object of Product is not
+        an instance of Category 
+        """
+
+        obj = Product.objects.get(pk=1)
+
+        self.assertNotIsInstance('Dummy data', Product)
+        self.assertNotEqual(obj.__str__(), 'Dummy data')
+
+    def test_data_for_instance(self):
+        """
+        Testing an instance data is correct
+        """
+        ins = Product.objects.get(pk=1)
+
+        self.assertEqual(ins.name, 'Testing Product')
+        self.assertEqual(ins.price, 100.00)
+        self.assertNotIsInstance(type(ins.price), float)
+
+    def test_get_absolute_url_method(self):
+        """
+        Test should return a url route
+        """
+        obj = Product.objects.get(pk=1)
+
+        self.assertEqual(
+            obj.get_absolute_url(), '/1/testing-product/'
+        )
+
+    def test_discount_method(self):
+        """
+        Test should return the field discount's value
+        """
+        obj = Product.objects.get(pk=1)
+
+        self.assertEqual(obj.discount, obj._discount)
+
+    def test_image_method(self):
+        """
+        Test should return a file path
+        """
+        obj = Product.objects.get(pk=1)
+        obj._image = 'image.png'
+        obj.save()
+
+        self.assertEqual(obj.image, '/uploads/image.png')
+
+    def test_no_image_for_image_method(self):
+        """
+        Test should return an empty string
+        """
+        obj = Product.objects.get(pk=1)
+
+        self.assertEqual(obj.image, '')
