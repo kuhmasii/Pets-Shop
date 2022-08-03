7a9a1cadb975d09d8206c7ee776acdf27ddfb378
kuhmasii <olaisaiah54@gmail.com>
2022-08-03 13:58:56 +0100

    updated content in setting file

--- petshop/petshop/settings.py
+++ petshop/petshop/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'petshop.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DATABASE"),
+        "USER": env.str("USER_NAME"),
+        "PASSWORD": env.str("PASSWORD"),
+        "HOST": env.str("HOST"),
+        "PORT": env.str("PORT"),
     }
 }
 
